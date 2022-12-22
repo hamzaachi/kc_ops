a653ea65264f4e4b759d4b0ee7e6edf36b528a23
hamza.achi <hamza.achi@eadn.dz>
2022-12-22 21:10:22 +0100

    refactoring

---
+++ roles.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Nerzal/gocloak/v12"
+)
+
+func (c *Instance) GetRealmRole(Name string, ctx context.Context, token string, realm string, source *gocloak.GoCloak) (*gocloak.Role, error) {
+	Role, err := source.GetRealmRole(ctx, token, realm, Name)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot get Role Info Error: %s", err)
+	}
+	return Role, nil
+}
+
+func (c *Instance) AddRealmRole(ctx context.Context, token string, target *gocloak.GoCloak, Role *gocloak.Role) error {
+	_, err := target.CreateRealmRole(ctx, token, c.Kc_target.Realm, *Role)
+	if err != nil {
+		return fmt.Errorf("Cannot Create Realm Role Error: %s", err)
+	}
+	return nil
+}
